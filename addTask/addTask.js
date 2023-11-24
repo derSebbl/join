e50880d1552d53e106610892072a1b3daa4ea725
Sebastian <139996363+derSebbl@users.noreply.github.com>
2023-11-24 20:54:59 +0100

    change css to hover over subtask

--- addTask/addTask.js
+++ addTask/addTask.js
@@ -10,35 +10,6 @@ async function initAddTask() {
 };
 
 /**
- * Eventlistener to change the pictures of the subtask input field
- * 
- */
-document.addEventListener('DOMContentLoaded', function() {
-  let inputField = document.getElementById("subtaskInput");
-  let imagePlus = document.getElementById("subtaskImgPlus");
-  let ImageXandAc = document.getElementById("subtaskImg");
-
-  inputField.addEventListener('input', function() {
-    if (inputField.value !== '') {
-      imagePlus.style.display = 'none';
-      ImageXandAc.style.display = 'flex';
-    } else {
-      imagePlus.style.display = 'block';
-      ImageXandAc.style.display = 'none';
-    }
-  });
-});
-
-/**
- * Function to prevent the reloading of the page when the form is submitted
- *
- */
-document.getElementById("formContainer").addEventListener("submit", function (event) {
-    event.preventDefault();
-    addToTask();
-  }); 
-
-/**
  * Function to push the new Task to the array and to the local storage
  *
  */
