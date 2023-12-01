48c9fcc5cdd3680a10935fa35d612eca2bb20358
Sebastian <139996363+derSebbl@users.noreply.github.com>
2023-12-01 21:44:43 +0100

    added FrameColor if active

--- Templates/Template.js
+++ Templates/Template.js
@@ -1,65 +1,72 @@
-/*async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
+let pages = [
+    { id: "summaryMenu", url: "/summary/summary.html" },
+    { id: "addTaskMenu", url: "/addTask/addTask.html" },
+    { id: "boardMenu", url: "/board/board.html" },
+    { id: "contactMenu", url: "/contact/contact.html" },
+];
+
+let checkExist = setInterval(function () {
+    for (var i = 0; i < pages.length; i++) {
+        let page = pages[i];
+        let element = document.getElementById(page.id);
+        if (element && window.location.href.endsWith(page.url)) {
+            element.className = "";
+            element.classList.add("active");
+            clearInterval(checkExist);
+        } else if (element) {
+            element.className = "";
         }
-    }*/ //dieser Code muss in jede script Datei selbst eingebunden werden 
+    }
+}, 20);
 
-function toSummary(){
-    window.location.href = '/summary/summary.html';
-};
 
-function toAddTask(){
-    window.location.href = '/addTask/addTask.html';
-};
+function toSummary() {
+  window.location.href = "/summary/summary.html";
+}
 
-function toBoard(){
-    window.location.href = '/board/board.html';
-};
+function toAddTask() {
+  window.location.href = "/addTask/addTask.html";
+}
 
-function toContact(){
-    window.location.href = '/contact/contact.html';
-};
+function toBoard() {
+  window.location.href = "/board/board.html";
+}
+
+function toContact() {
+  window.location.href = "/contact/contact.html";
+}
 
 function toPrivacyPolicy() {
-    window.open('../privacyPolicy.html', '_blank');
-};
+  window.open("../privacyPolicy.html", "_blank");
+}
 
 function toLegalNotice() {
-    window.open('../legalNotice.html', '_blank');
+  window.open("../legalNotice.html", "_blank");
 }
 
 let closeMenuOnClickOutsideBound;
 
 function closeMenuOnClickOutside(miniMenu, event) {
-    if (!miniMenu.contains(event.target)) {
-        miniMenu.style.display = "none";
-        document.removeEventListener('click', closeMenuOnClickOutsideBound);
-    }
-};
+  if (!miniMenu.contains(event.target)) {
+    miniMenu.style.display = "none";
+    document.removeEventListener("click", closeMenuOnClickOutsideBound);
+  }
+}
 
 function openMiniMenu() {
-    let miniMenu = document.getElementById("minimenu");
-    if (miniMenu.style.display === "none") {
-        miniMenu.style.display = "inline-flex";
-        closeMenuOnClickOutsideBound = closeMenuOnClickOutside.bind(null, miniMenu);
-        setTimeout(() => {
-            document.addEventListener('click', closeMenuOnClickOutsideBound);
-        }, 0);
-    } else {
-        miniMenu.style.display = "none";
-        document.removeEventListener('click', closeMenuOnClickOutsideBound);
-    }
-};
+  let miniMenu = document.getElementById("minimenu");
+  if (miniMenu.style.display === "none") {
+    miniMenu.style.display = "inline-flex";
+    closeMenuOnClickOutsideBound = closeMenuOnClickOutside.bind(null, miniMenu);
+    setTimeout(() => {
+      document.addEventListener("click", closeMenuOnClickOutsideBound);
+    }, 0);
+  } else {
+    miniMenu.style.display = "none";
+    document.removeEventListener("click", closeMenuOnClickOutsideBound);
+  }
+}
 
 function goBack() {
-    window.close();
-};
-
-
+  window.close();
+}
