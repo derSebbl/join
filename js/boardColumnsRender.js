db775cdbf8b5927981a2f7fd43457befe70269be
Sebastian <139996363+derSebbl@users.noreply.github.com>
2023-12-05 20:52:13 +0100

    Update boardColumnsRender.js

--- js/boardColumnsRender.js
+++ js/boardColumnsRender.js
@@ -2,6 +2,11 @@ window.addEventListener('resize', function() {
     ChangeParentsResp();
 });
 
+/**
+ * Renders the title container for each board column.
+ * The title container includes the column title and a plus button for adding new tasks.
+ * 
+ */
 function renderBoardColumnTitleContainer() {
     let outputColumnTitle = '';
     let columnTitle;
@@ -13,23 +18,19 @@ function renderBoardColumnTitleContainer() {
                 columnTitle = 'To do';
                 id = 'To do';
                 break;
-
             case 1:
                 columnTitle = 'In progress';
                 id = 'In progress';
                 break;
-
             case 2:
                 columnTitle = 'Await feedback';
                 id = 'Await feedback';
                 break;
-
             case 3:
                 columnTitle = 'Done';
                 id = 'Done';
                 plusButtonIcons = '';
                 break;
-
             default:
                 break;
         }
@@ -43,8 +44,15 @@ function renderBoardColumnTitleContainer() {
                             `;
     }
     document.getElementById('boardColumnTitleContainer').innerHTML = outputColumnTitle;
-}
+};
 
+/**
+ * Renders the plus button icons for each board column.
+ * The plus button is used to create new tasks in the column.
+ *
+ * @param {number} i - The index of the board column.
+ * @returns {string} A string representing the HTML for the plus button icons.
+ */
 function renderPlusButtonIcons(i) {
     return `
                 <div class="plusButtonIcons" onclick="createNewTaskInColumn(${i})">
@@ -55,54 +63,58 @@ function renderPlusButtonIcons(i) {
                     <div class="plusButtonIconWhileClick"><img class="plusButtonIconWhileClickImage"
                             src="../assets/icons/plusButtonIconWhileClick.svg" alt="Add"></div>
                 </div>
-
             `;
-} 
+};
 
+/**
+ * Renders the board columns.
+ * Each column is a drop zone for tasks and has an associated ondrop, ondrag, ondragleave, and ondragover event.
+ * 
+ */
 function renderBoardColumns() {
     let outputColumns = '';
     for (let i = 0; i < boardColumn.length; i++) {
         outputColumns += `<div id="${boardColumn[i]}" class="boardTaskColumn" ondrop="moveTo('${boardColumn[i]}'); setTasksData()" ondrag="highlight(${i})" ondragleave="removeHighlight(${i})" ondragover="allowDrop(event)"></div>`;
     }
     document.getElementById('boardColumnContainer').innerHTML = outputColumns;
-}
+};
 
-function ChangeParentsResp(){
-    let ToDoHeader = document.getElementById('To do');
-    let InProgressHeader = document.getElementById('In progress');
-    let AwaitFeedbackHeader = document.getElementById('Await feedback');
-    let DoneHeader = document.getElementById('Done');
-    let ToDoColumn = document.getElementById('boardColumnToDo');
-    let InProgressColumn = document.getElementById('boardColumnInProgress');
-    let AwaitFeedbackColumn = document.getElementById('boardColumnAwaitFeedback');
-    let DoneColumn = document.getElementById('boardColumnDone');
-    let boardHeader = document.getElementById('boardHeader');
-    let search = document.getElementById('search');
+/**
+ * Changes the parent elements of the board columns and search bar based on the window width.
+ * If the window width is less than 1420, each board column is appended to its corresponding header and the search bar is appended to the board header.
+ * If the window width is 1420 or greater, the ChangeParentsRespBack function is called.
+ * 
+ */
+function ChangeParentsResp() {
+    const headers = ['To do', 'In progress', 'Await feedback', 'Done'];
+    const columns = ['boardColumnToDo', 'boardColumnInProgress', 'boardColumnAwaitFeedback', 'boardColumnDone'];
 
-    if(window.innerWidth < 1420){
-    ToDoHeader.appendChild(ToDoColumn);
-    InProgressHeader.appendChild(InProgressColumn);
-    AwaitFeedbackHeader.appendChild(AwaitFeedbackColumn);
-    DoneHeader.appendChild(DoneColumn);
-    boardHeader.appendChild(search);
+    if (window.innerWidth < 1420) {
+        headers.forEach((header, index) => {
+            document.getElementById(header).appendChild(document.getElementById(columns[index]));
+        });
+        document.getElementById('boardHeader').appendChild(document.getElementById('search'));
     } else {
         ChangeParentsRespBack();
     }
 };
 
-function ChangeParentsRespBack(){
-    let MainColumnContainer = document.getElementById('boardColumnContainer');
-    let searchAndAddTask = document.getElementById('searchAndAddTask');
-    let ToDoColumn = document.getElementById('boardColumnToDo');
-    let InProgressColumn = document.getElementById('boardColumnInProgress');
-    let AwaitFeedbackColumn = document.getElementById('boardColumnAwaitFeedback');
-    let DoneColumn = document.getElementById('boardColumnDone');
-    let search = document.getElementById('search');
+/**
+ * Reverts the changes made by the ChangeParentsResp function.
+ * Each board column is appended back to the main column container and the search bar is appended back to the search and add task container.
+ * 
+ */
+function ChangeParentsRespBack() {
+    const mainColumnContainer = document.getElementById('boardColumnContainer');
+    const searchAndAddTask = document.getElementById('searchAndAddTask');
+    const columns = ['boardColumnToDo', 'boardColumnInProgress', 'boardColumnAwaitFeedback', 'boardColumnDone'];
 
-    MainColumnContainer.appendChild(ToDoColumn);
-    MainColumnContainer.appendChild(InProgressColumn);
-    MainColumnContainer.appendChild(AwaitFeedbackColumn);
-    MainColumnContainer.appendChild(DoneColumn);
+    columns.forEach(column => {
+        const columnElement = document.getElementById(column);
+        mainColumnContainer.appendChild(columnElement);
+    });
+
+    const search = document.getElementById('search');
     searchAndAddTask.appendChild(search);
 };
 
