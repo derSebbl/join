70956dd7b14f5fc230b85da213846664dbbf80bc
Sebastian <139996363+derSebbl@users.noreply.github.com>
2023-12-05 20:54:47 +0100

    Update designTemplatesGeneralElements.js

--- js/designTemplatesGeneralElements.js
+++ js/designTemplatesGeneralElements.js
@@ -1,24 +1,16 @@
-function initProfileBadgesUser() {
-}
-
-function taskSearchString() {
-    return
-}
-
-function renderButtonsForPriority() {
-}
-
-
+/**
+ * Sets the active button for the priority.
+ *
+ * @param {number} setPriority - The priority to be set.
+ */
 function setActiveButtonForPriorityTo(setPriority) {
     let setClassForButton = `priorityButton${cardPriority[setPriority]}`;
     let setClassForButtonIcon = `priorityButton${cardPriority[setPriority]}Icon`;
 
-    // resetPriorityButton
     document.getElementById(setClassForButton).classList.remove(`buttonForPriority`);
     document.getElementById(setClassForButtonIcon).classList.remove(`buttonForPriorityIcon${cardPriority[setPriority]}`);
     document.getElementById(setClassForButtonIcon).classList.remove(`OverlayPriorityIcon`);
-
     document.getElementById(setClassForButton).classList.add(`buttonActiveForPriority${cardPriority[setPriority]}`);
     document.getElementById(setClassForButtonIcon).classList.add(`OverlayPriorityIcon${cardPriority[setPriority]}White`);
     document.getElementById(setClassForButtonIcon).classList.add(`buttonForPriorityIcon`);
-}
+};
